951abe71ba1a894a943faa7bdabf82cfeed71a8c
conglh2610 <lhcongtk32@gmail.com>
2018-03-05 23:10:23 +0700

    test

--- A2Sample/src/app/web-components/auto-complete/auto-complete.component.ts
+++ A2Sample/src/app/web-components/auto-complete/auto-complete.component.ts
@@ -186,7 +186,6 @@ export class AutoCompleteComponent implements OnChanges {
     }
 
     $(".auto-complete-content").css("display", "block");
-    //event.stopPropagation();
   }
 
   searchTextFocusOut(e) {
